a12a1dc89ba1316fd5574ad808637fb01e8909d6
Jose matheus <j.matheus.v.souza@gmail.com>
2025-04-22 08:31:43 -0300

    desafio 1 feito

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+a = int(input("informe A: "))
+b = int(input("informe b: "))
+c = int(input("informe c: "))
+soma = a + b
+if soma > c:
+    print(f"o resultado de a + b é {soma}, sendo assim maior que {c}.")
+else:
+    print(f"o resultado de a + b é {soma}, sendo c igual a {c} sendo assim maior.")
